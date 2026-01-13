dd56a80763f0416f79f56abd534b62ae10e6cd39
k.ms <k.ms@astkorea.net>
2026-01-13 09:46:08 +0900

    first commit

---
+++ src/test/java/com/api/ast/apigatewayservice/ApigatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ast.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
